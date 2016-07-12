7218f954b6295aee6410a138238e69860c0658c4
Robin Claes <Robin Claes>
2016-07-12 11:07:23 +0200

    project setup

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+gulp.task('default', function() {
+  browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch('src/*.html').on('change', browserSync.reload);
+    gulp.watch('src/*.js').on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("styles"))
+        .pipe(browserSync.stream());
+});
